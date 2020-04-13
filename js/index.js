a8bf8c6845df6b2475e2b0e3e575af952dc6da8e
Unknown <mehjabeen.shachi@gmail.com>
2020-04-13 13:22:52 +0600

    connected to firebase

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAQRL9VYRxk62JOrMQyxziL_6apBTkrAwk",
+    authDomain: "jolojdb.firebaseapp.com",
+    databaseURL: "https://jolojdb.firebaseio.com",
+    projectId: "jolojdb",
+    storageBucket: "jolojdb.appspot.com",
+    messagingSenderId: "896982534436",
+    appId: "1:896982534436:web:9e9ec95e67ae3de8128bb1",
+    measurementId: "G-3GQ64JSSKB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
