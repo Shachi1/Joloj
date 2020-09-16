d8d06014a5b5b1e870ba67b59dbae133f8180521
Mehjabeen Shachi <mehjabeen.shachi@gmail.com>
2020-09-16 15:00:39 +0600

    model updated

--- models/profile.js
+++ models/profile.js
@@ -8,7 +8,7 @@ const post = require('./post')
 const profileSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
-        ref: User,
+        ref: user,
         required: true
     },
     title: {
