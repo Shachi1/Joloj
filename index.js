b9843820091819a479af0a8cbbab2906ec7fa86f
Siddiqui-2583 <nurnaby83@gmail.com>
2020-09-16 09:42:32 +0600

    Project set up done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-// const MongoClient = require('mongodb').MongoClient;
 const mongoose=require('mongoose')
 require('dotenv').config()
 
@@ -13,79 +12,44 @@ app.use(cors())
 app.use(bodyParser.json())
 
 
-// const dbUser = 'dbUser'
-// const pass = 'Yrm1sdrmp9GZMOLK'
+const dbUser = 'dbUser'
+const pass = 'Yrm1sdrmp9GZMOLK'
 
-const dbUser = process.env.DB_USER
-const pass = process.env.DB_PASS
+// const dbUser = process.env.DB_USER
+// const pass = process.env.DB_PASS
 const uri = `mongodb+srv://${dbUser}:${pass}@cluster0.evhow.mongodb.net/joloj?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-// app.get('/get', (req, res) => {
-//     client.connect(err => {
-//         const collection = client.db("joloj").collection("dashboard");
-//         collection.find().toArray((err, documents) => {
-//             if (err) {
-//                 console.log(err)
-//                 res.status(500).send({ message: err })
-//             }
-//             else {
-//                 res.send(documents)
-//             }
-//         })
-//         //client.close()
-//     })
-// })
-
-// app.post('/post', (req, res) => {
-//     // console.log(req.body)
-//     //ekhane db te pathano hobe. then db theke id sohokare data ta ekhane send kora hobe
-//     const product = req.body
-//     //console.log(product)
-//     client.connect(err => {
-//         const collection = client.db("joloj").collection("dashboard");
-//         collection.insertOne({ product }, (err, result) => {
-//             if (err) {
-//                 console.log(err)
-//                 res.status(500).send({ message: err })
-//             }
-//             else {
-//                 res.send(result.ops[0])
-//             }
-
-
-//         })
-//     })
-//     // client.close();
-// });
-
-
-
-app.get('/about', (req, res) => {
-    res.render('pages/about')
+let Schema = mongoose.Schema
+let testSchema = new Schema({
+    name: String
 })
-app.get('/help', (req, res) => {
-    res.render('pages/help')
-})
+
+let Test = mongoose.model('Test', testSchema)
+
 app.get('/', (req, res) => {
-    let post = {
-        title: 'Test title',
-        body: 'Test body',
-        published: false
-    }
+    res.json({
+        message:"Hello"
+    })
+    // let test = new Test({
+    //     name:'Shourav'
+    // })
 
-    let posts = [
-        { title: 'title one', author: 'Shourav' },
-        { title: 'title two', author: 'Shourav' },
-        { title: 'title three', author: 'Shourav' },
-        { title: 'title four', author: 'Shourav' }
-    ]
-
-    res.render('pages/index', { title: 'EJs is cool!', post, posts })
+    // test.save()
+    //     .then(t => {
+    //         res.json(t)
+    //     })
+    //     .catch(e => {
+    //         console.log(e)
+    //         res.status(500).json({
+    //             error:"Error occured."
+    //         })
+    //     })
+    //  res.render('pages/index')
 })
 
-const PORT = process.env.PORT || 4200
+
+
+const PORT = process.env.PORT || 8080
 mongoose
     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
