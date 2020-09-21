7ce446cdce7a588340c524ba36ec8d501bff200a
Mehjabeen Shachi <mehjabeen.shachi@gmail.com>
2020-09-21 23:28:34 +0600

    code cleaning done

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -11,7 +11,6 @@ const {
     dislikesGetController
 } = require('../controllers/likeDislikeController')
 
-const { bookmarksGetController } = require('../controllers/bookmarkController')
 
 router.post('/comments/:postId', isAuthenticated, commentPostController)
 router.post('/comments/replies/:commentId', isAuthenticated, replyCommentPostController)
@@ -19,7 +18,6 @@ router.post('/comments/replies/:commentId', isAuthenticated, replyCommentPostCon
 router.get('/likes/:postId', isAuthenticated, likesGetController)
 router.get('/dislikes/:postId', isAuthenticated, dislikesGetController)
 
-router.get('/bookmarks/:postId', isAuthenticated, bookmarksGetController)
 
 
 
